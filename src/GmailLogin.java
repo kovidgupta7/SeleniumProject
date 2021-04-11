8ee0e530714d44fc94473302d5b513387efde4ad
kovid <kovidgupta1993@gmail.com>
2021-04-11 16:35:42 +0530

    Kovids first commit

---
+++ src/GmailLogin.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GmailLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.setProperty("webdriver.chrome.driver", "E:\\KOVID\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://mail.google.com/");
+		driver.manage().window().maximize();
+       
+	}
+
+}
