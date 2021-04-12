ae9e9952823ce09a9acbe2fcac4b1ebd0724fbe1
kovidgupta7 <kovidgupta1993@gmail.com>
2021-04-12 22:39:41 +0530

    final

--- src/GmailAddress.java
+++ src/GmailAddress.java
@@ -1,9 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GmailAddress {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "E:\\KOVID\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://mail.google.com/");
+		driver.manage().window().maximize();
 	}
 
 }
